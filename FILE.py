68308010186d5b86f22376724fb40c8744f0ee0e
Codine 2 <54627870+islamtiaib@users.noreply.github.com>
2022-12-15 11:58:46 +0100

    Add files via upload

---
+++ FILE.py
@@ -0,0 +1,22 @@
+import os, platform, time
+os.system('git pull -q')
+print('')
+exit()
+yy = input('Did You Know How to Make EaaB Token??(y/n)')
+if yy in 'yes Y y Yes YES':
+ pass
+else:
+ os.system('xdg-open https://www.facebook.com/100074059501726/posts/150776190734364/?app=fbl')
+uu = os.popen('play').read()
+
+if 'Usage summary' in uu:
+ pass
+else:
+ print(uu)
+ os.system('pkg install sox -y')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+ import FILE64
+elif bit == '32bit':
+ import FILE32
